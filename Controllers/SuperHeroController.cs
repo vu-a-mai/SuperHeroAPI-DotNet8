7c331a46c7f533f1337112934868e06e3e13341f
Vu Mai <vuanhmai.io@gmail.com>
2024-03-12 18:19:00 -0700

    Add a custom SuperHeroController API

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI_DotNet8.Entities;
+
+namespace SuperHeroAPI_DotNet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            // initialise a list to store SuperHero
+            var heroes = new List<SuperHero>
+            {
+                // add a Super Hero
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spiderman",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+
+            // Return Status Code: 200
+            return Ok(heroes);
+        }
+    }
+}
