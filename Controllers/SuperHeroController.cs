92bc6582a297a18a1a60a9e4ad6c9531e158145d
Vu Mai <vuanhmai.io@gmail.com>
2024-03-12 20:33:53 -0700

    Retrieving Data API  using {id} to Retrieved a single entry

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -22,29 +22,30 @@ namespace SuperHeroAPI_DotNet8.Controllers
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
         {
-            /*
-            // initialise a list to store SuperHero
-            var heroes = new List<SuperHero>
-            {
-
-                // add a Super Hero
-                new SuperHero
-                {
-                    Id = 1,
-                    Name = "Spiderman",
-                    FirstName = "Peter",
-                    LastName = "Parker",
-                    Place = "New York City"
-                }
-            };
-            */
-
             // Access SuperHeroes Table
             // Turn it to a List
             var heroes = await _context.SuperHeroes.ToListAsync();
 
             // Return Status Code: 200
             return Ok(heroes);
+        }
+
+        // Route id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> GetHero(int id)
+        {
+            // Find the hero based on the id
+            var hero = await _context.SuperHeroes.FindAsync(id);
+
+            // if hero is not found
+            if(hero is null)
+            {
+                // Return Status Code: 404
+                return NotFound("Hero not found");
+            }
+
+            // Return Status
+            return Ok(hero);
         }
     }
 }
