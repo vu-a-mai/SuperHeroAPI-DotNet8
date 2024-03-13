eb6cf0c3aec4901e7b3f168ea3fc15b54f17795e
Vu Mai <vuanhmai.io@gmail.com>
2024-03-12 20:07:11 -0700

    - Inject Data Context into SuperHeroesController Service Repository - Updated HTTP Get API to use context to access SuperHeroes Table in the database and turn it a List

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_DotNet8.Data;
 using SuperHeroAPI_DotNet8.Entities;
 
 namespace SuperHeroAPI_DotNet8.Controllers
@@ -8,12 +10,23 @@ namespace SuperHeroAPI_DotNet8.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        // inject data context
+        private readonly DataContext _context;
+
+        // inject data context into the service repository
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
         {
+            /*
             // initialise a list to store SuperHero
             var heroes = new List<SuperHero>
             {
+
                 // add a Super Hero
                 new SuperHero
                 {
@@ -24,6 +37,11 @@ namespace SuperHeroAPI_DotNet8.Controllers
                     Place = "New York City"
                 }
             };
+            */
+
+            // Access SuperHeroes Table
+            // Turn it to a List
+            var heroes = await _context.SuperHeroes.ToListAsync();
 
             // Return Status Code: 200
             return Ok(heroes);
