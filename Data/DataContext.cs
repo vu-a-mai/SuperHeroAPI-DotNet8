83b9c1441217497a53311e38754e057233e91cf7
Vu Mai <vuanhmai.io@gmail.com>
2024-03-12 19:09:22 -0700

    Install Microsoft.EntityFrameworkCore and add DBContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_DotNet8.Entities;
+
+namespace SuperHeroAPI_DotNet8.Data
+{
+    // Ctrl + . on DbContext for quickfix
+    // Install the latest package 'Microsoft.EntityFrameworkCore
+    // Inherit from DBContext
+    public class DataContext : DbContext
+    {
+        // Boiler plate code
+        // Constructor
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+
+        }
+
+        // Database Set
+        public DbSet<SuperHero> SuperHeroes{ get; set; }
+    }
+}
