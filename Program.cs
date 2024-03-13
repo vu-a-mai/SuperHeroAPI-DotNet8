8ac027a5739ebfea71bf38cec0b7557eb0ebc99c
Vu Mai <vuanhmai.io@gmail.com>
2024-03-12 19:28:57 -0700

    - Install Microsoft.EntityFrameworkCore.SqlServer version 8 through NuGet Packages - Add AddDBContext Services to use SQL Server Database configuration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_DotNet8.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// For AddDbContext Services
+// Right-click Project, Manage NuGet Packages
+// Search for Microsoft.EntityFrameworkCore.SqlServer version 8 and install
+// UseSQLServer squiggly line, Ctrl + . to add "using Microsoft.EntityFrameworkCore;
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -16,6 +28,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Middlewares
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
